376a086b33f6b24bae819356d42168268699c17f
Gaanj <Dmytrobozescyl@gmail.com>
2023-12-03 14:13:35 +0200

    upd

--- js/task-2.js
+++ js/task-2.js
@@ -1,13 +1,33 @@
-let firstArray = [];
-let secondArray = [];
-function makeArray(firstArray, secondArray, maxLength){
-    let concatMass = firstArray.concat(secondArray);
-    let sliceMass = concatMass.slice(0,maxLength);
-    return sliceMass;
+function calcAverageCalories(days) {
+    if (days.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0; i < days.length; i++) {
+        sum += days[i].calories;
+    }
+    return sum / days.length;
 }
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+console.log(
+    calcAverageCalories([
+        { day: "monday", calories: 3010 },
+        { day: "tuesday", calories: 3200 },
+        { day: "wednesday", calories: 3120 },
+        { day: "thursday", calories: 2900 },
+        { day: "friday", calories: 3450 },
+        { day: "saturday", calories: 3280 },
+        { day: "sunday", calories: 3300 }])
+); 
+console.log(
+    calcAverageCalories([
+        { day: "monday", calories: 2040 },
+        { day: "tuesday", calories: 2270 },
+        { day: "wednesday", calories: 2420 },
+        { day: "thursday", calories: 1900 },
+        { day: "friday", calories: 2370 },
+        { day: "saturday", calories: 2280 },
+        { day: "sunday", calories: 2610 }])
+);
+console.log(
+    calcAverageCalories([])
+);
--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,18 @@
-// Функція filterArray приймає масив чисел (numbers) та значення (value) як параметри
-function filterArray(numbers, value) {
-  // Створюємо новий порожній масив для результату
-  let result = [];
-  // Проходимо по всіх елементах масиву numbers
-  for (let number of numbers) {
-    // Якщо число більше за value, додаємо його до масиву result
-    if (number > value) {
-      result.push(number);
-    }
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
   }
-  // Повертаємо масив result
-  return result;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3));
-console.log(filterArray([1, 2, 3, 4, 5], 4));
-console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+};
+console.log(profile.getInfo());
+profile.changeUsername("Marco");
+console.log(profile.getInfo());
+profile.updatePlayTime(20);
+console.log(profile.getInfo());
